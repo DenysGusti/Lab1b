e86244040173801df85850c970376e828cf18c79
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2025-04-16 03:21:16 +0200

    fixed rotations

--- src/transformation_object.js
+++ src/transformation_object.js
@@ -3,36 +3,32 @@ import * as glm from './gl-matrix/index.js';
 // parent class for all objects that will be transformed
 export class TransformationObject {
     scalingMatrix = glm.mat4.create();
-    rotationMatrix = glm.mat4.create();
-    translationMatrix = glm.mat4.create();
+    rotationTranslationMatrix = glm.mat4.create();
 
     scale(scaleVec) {
         glm.mat4.scale(this.scalingMatrix, this.scalingMatrix, scaleVec);
     }
 
     rotateX(angle) {
-        glm.mat4.rotateX(this.rotationMatrix, this.rotationMatrix, angle);
+        glm.mat4.rotateX(this.rotationTranslationMatrix, this.rotationTranslationMatrix, angle);
     }
 
     rotateY(angle) {
-        glm.mat4.rotateY(this.rotationMatrix, this.rotationMatrix, angle);
+        glm.mat4.rotateY(this.rotationTranslationMatrix, this.rotationTranslationMatrix, angle);
     }
 
     rotateZ(angle) {
-        glm.mat4.rotateZ(this.rotationMatrix, this.rotationMatrix, angle);
+        glm.mat4.rotateZ(this.rotationTranslationMatrix, this.rotationTranslationMatrix, angle);
     }
 
     translate(translateVec) {
-        glm.mat4.translate(this.translationMatrix, this.translationMatrix, translateVec);
+        glm.mat4.translate(this.rotationTranslationMatrix, this.rotationTranslationMatrix, translateVec);
     }
 
     // R * T * S
     getTransformationMatrix() {
         const modelMatrix = glm.mat4.create();
-
-        glm.mat4.multiply(modelMatrix, this.translationMatrix, this.scalingMatrix);
-        glm.mat4.multiply(modelMatrix, this.rotationMatrix, modelMatrix);
-
+        glm.mat4.multiply(modelMatrix, this.rotationTranslationMatrix, this.scalingMatrix);
         return modelMatrix;
     }
 }
